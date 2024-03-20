f50737de7d01413feda3d27725870332863cd735
Nikhil Rustagi <nikhilrstg18@gmail.com>
2024-03-20 16:35:52 +0530

    Added google font with gatsby-omni-font-loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,17 @@ module.exports = {
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },{
+    resolve: `gatsby-omni-font-loader`,
+    options: {
+      enableListener: true,
+      preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+      web: [
+        {
+          name: `Roboto`,
+          file: `https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap`,
+        },
+      ],
+    },
+  },]
 };
